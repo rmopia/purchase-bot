bb092e64e887b7f1f74e412c5996fd7e95f10033
Robert Mopia <robert.mopia@gmail.com>
2020-05-13 11:37:28 -0700

    Added autofiller for personal info and new method to get url

--- purchasebot.py
+++ purchasebot.py
@@ -1,11 +1,7 @@
-import threading
 import time
 import requests
 import json
-from datetime import datetime
-from datetime import timedelta
 import schedule
-import self as self
 from selenium import webdriver
 
 
@@ -18,20 +14,33 @@ def check_product_test():
 
     for pr in products:
         if pr['title'] == 'Mesh Beach Tote Bag':
-            # print(pr['variants'['title']])
-            return 'https://www.stussy.com/collections/backpacks-and-bags/products/' + pr['handle']
+            for var in pr['variants']:
+                if 'EA' in var['title']:
+                    print(str(var['id']) + " " + var['title'])
+                    return 'https://www.stussy.com/collections/backpacks-and-bags/products/' + pr['handle'] \
+                           + '?variant=' + str(var['id'])
 
     return False
 
 
 def purchase_test(url):
     driver.get(url)
-    driver.find_element_by_xpath('//div[@class="swatch__element ea"]').click()
-    driver.find_element_by_xpath('//button[@class="btn product__atc"]').click()
-    time.sleep(1.3)
-    driver.find_element_by_xpath('//button[@class="btn ajaxcart__checkout"]').click()
-    while 1:
-        continue
+
+    element = driver.find_element_by_xpath('//button[@class="btn product__atc"]')
+    if element.is_enabled():
+        element.click()
+        time.sleep(0.5)
+        driver.find_element_by_xpath('//button[@class="btn ajaxcart__checkout"]').click()
+        time.sleep(0.5)
+        driver.execute_script('document.getElementById("checkout_email_or_phone").value="sample@gmail.com";')
+        driver.execute_script('document.getElementById("checkout_shipping_address_first_name").value="Chester";')
+        driver.execute_script('document.getElementById("checkout_shipping_address_last_name").value="Tester";')
+        driver.execute_script('document.getElementById("checkout_shipping_address_address1").value="123 Apple Street";')
+        driver.execute_script('document.getElementById("checkout_shipping_address_city").value="San Marcos";')
+        driver.execute_script('document.getElementById("checkout_shipping_address_zip").value="91912";')
+        driver.find_element_by_xpath('//button[@class="step__footer__continue-btn btn"]').click()
+    else:
+        print('oopsy')
 
 
 def check_product():
@@ -40,14 +49,16 @@ def check_product():
 
     for pr in products:
         if pr['title'] == 'Stüssy / Nike Air Zoom Spiridon Cage 2' and pr['handle'] != 'afqh9nnue888ffv':
-            # print(pr['variants'['title']])
-            return 'https://www.stussy.com/collections/nike/products/' + pr['handle']
+            for var in pr['variants']:
+                if "8" in var['title']:
+                    print(str(var['id']) + " " + var['title'])
+                    return 'https://www.stussy.com/collections/nike/products/' + pr['handle'] \
+                           + '?variant=' + str(var['id'])
 
     return False
 
 
 def purchase(url):
-    driver = webdriver.Chrome(executable_path=r'C:\Users\heybo\Desktop\chromedriver.exe')
     driver.get(url)
     driver.find_element_by_xpath('//div[@data-value="8"]').click()
 
@@ -59,4 +70,12 @@ def handler():
     else:
         print("oops")
 
+
 handler()
+
+# schedule.every().day.at("11:06:02").do(handler)
+# schedule.every().day.at("10:00:00").do(handler)
+
+'''while True:
+    schedule.run_pending()
+    time.sleep(0.5)'''
